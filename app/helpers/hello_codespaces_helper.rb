5225432b0d6925d169c240d9a21c2f5405ff01f7
LuisAngelTgn <82842998+LuisAngelTgn@users.noreply.github.com>
2023-03-05 18:14:36 +0000

    Pending changes exported from your codespace

--- app/helpers/hello_codespaces_helper.rb
+++ app/helpers/hello_codespaces_helper.rb
@@ -1,2 +1,23 @@
 module HelloCodespacesHelper
-end
+    def divide_array(array, num_groups)
+      result = []
+      group_size = array.size / num_groups
+      remainder = array.size % num_groups
+      start_index = 0
+  
+      num_groups.times do |i|
+        group = []
+        group_size.times do |j|
+          group << array[start_index + j*num_groups]
+        end
+  
+        group << array[start_index + group_size*num_groups] if i < remainder
+  
+        result << group
+        start_index += 1
+      end
+  
+      result
+    end
+  end
+  
